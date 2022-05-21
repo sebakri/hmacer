0707770ed0fb7ea1e092ce1f6025ca4f0c143fed
Sebastian Krieger <sebastian.krieger@auvesy-mdt.com>
2022-05-21 19:01:09 +0200

    Add cli to verify files

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 )
@@ -25,6 +26,16 @@ func main() {
 		log.Fatal(err)
 	}
 
+	args := flag.Args()
+	if len(args) > 0 && args[0] == "verify" {
+		if verify(fileBytes, *secret) {
+			fmt.Print("OK")
+		} else {
+			fmt.Print("FAIL")
+		}
+		return
+	}
+
 	h := hmac.New(sha256.New, []byte(*secret))
 
 	n, err := h.Write(fileBytes)
@@ -36,7 +47,16 @@ func main() {
 		log.Fatal(err)
 	}
 
-	if err := os.WriteFile(*fileName+".hmac", h.Sum(nil), 0644); err != nil {
+	if err := os.WriteFile(*fileName+".hmac", h.Sum(fileBytes), 0644); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func verify(bytes []byte, secret string) bool {
+	content := bytes[:len(bytes)-sha256.Size]
+	mac := bytes[len(bytes)-sha256.Size:]
+
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write(content)
+	return hmac.Equal(mac, h.Sum(nil))
 }
